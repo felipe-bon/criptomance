df9ca592b70a5ff889c78de67887f084b01e5933
felipe-bon <felipebonetti2001@hotmail.com>
2026-06-21 19:21:08 -0300

    add executing genetic algorithm tests

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 import crossover.CrossoverStrategy;
 import crossover.CyclicCrossover;
+import fitness.FitnessGlobal;
+import fitness.FitnessStrategy;
+import geneticAlgorithm.CryptoProblem;
 import geneticAlgorithm.GenAlg;
 import parentSelection.ParentSelectionStrategy;
 import parentSelection.TournamentSelection;
@@ -8,11 +11,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Iniciando sistema!");
 
+        CryptoProblem currenProblem = new CryptoProblem("send", "more", "money");
+
         CrossoverStrategy crossoverMethod = new CyclicCrossover();
         ParentSelectionStrategy selectionParentMethod = new TournamentSelection();
+        FitnessStrategy fitnessMethod = new FitnessGlobal(currenProblem);
 
-        GenAlg algoritmoGenetico = new GenAlg(20, 100, crossoverMethod, selectionParentMethod);
-        algoritmoGenetico.getPopulation();
+        GenAlg algoritmoGenetico = new GenAlg(20, 100, 50, 60, crossoverMethod, selectionParentMethod, fitnessMethod);
+        algoritmoGenetico.executeAlgorithm();
+
+        System.out.println(algoritmoGenetico.getLettersSequenc());
+
+        algoritmoGenetico.getPopulation().forEach(individual->{
+            System.out.println(individual);
+        });
 
         System.out.println("Encerrando sistema!");
     }
