2f50b207963111ad6b17ce5a0fb225b7a46ac91c
felipe-bon <felipebonetti2001@hotmail.com>
2026-06-21 19:19:51 -0300

    add calculate population fitness method, get letters sequence method and others atributes

--- src/geneticAlgorithm/GenAlg.java
+++ src/geneticAlgorithm/GenAlg.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import crossover.CrossoverStrategy;
+import fitness.FitnessStrategy;
 import parentSelection.ParentSelectionStrategy;
 
 public class GenAlg {
@@ -13,23 +14,29 @@ public class GenAlg {
   private ArrayList<Individual> population;
   private CrossoverStrategy crossoverStrategy;
   private ParentSelectionStrategy parentSelectionStrategy;
+  private FitnessStrategy fitnessStrategy;
+  private int maxGeneration = 100;
+  private int crossoverRate = 60;
 
 
-  public GenAlg(int p_mutation, int population_size, CrossoverStrategy crossoverStrategy, ParentSelectionStrategy parentSelectionStrategy){
+  public GenAlg(int p_mutation, int population_size, int maxGeneration, int crossoverRate, CrossoverStrategy crossoverStrategy, ParentSelectionStrategy parentSelectionStrategy, FitnessStrategy fitnessStrategy){
     this.crossoverStrategy = crossoverStrategy;
     this.parentSelectionStrategy = parentSelectionStrategy;
+    this.fitnessStrategy = fitnessStrategy;
     this.p_mutation = p_mutation;
     this.population_size = population_size;
+    this.maxGeneration = maxGeneration;
+    this.crossoverRate = crossoverRate;
+
     this.generatePopulation();
   }
 
   private void generatePopulation(){
+    // Gera população usando paralelismo
     this.population = IntStream.range(0, population_size)
     .parallel()
     .mapToObj(i -> new Individual(p_mutation))
     .collect(Collectors.toCollection(ArrayList::new));
-
-    System.out.println(population);
   }
 
   // TODO
@@ -44,19 +51,28 @@ public class GenAlg {
     return;
   }
 
-  // TODO
-  private void calculateFitness(Individual[] individuals){
+  // Calcula o fitness da população usando paralelismo
+  private void calculatePopulationFitness(){
     
+    this.population.parallelStream().forEach(individual -> {
+      int fitness = fitnessStrategy.calculateFitness(individual.getChromosome());
+      individual.setFitnessValue(fitness);
+    });
+
     return;
   }
 
   // TODO
-  private void executeAlgorithm(){
-
+  public void executeAlgorithm(){
+    calculatePopulationFitness();
     return;
   }
 
   public ArrayList<Individual> getPopulation(){
     return population;
+  }
+
+  public String getLettersSequenc(){
+    return this.fitnessStrategy.getLettersSequenc();
   }
 }
