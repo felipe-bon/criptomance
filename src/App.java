da0069f088405f6da5a53024fb5fdc967f7a22e8
felipe-bon <felipebonetti2001@hotmail.com>
2026-06-23 22:43:17 -0300

    add initial algorithm execution configuration

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import Reintegration.ReintegrationStrategy;
+import Reintegration.SortededReintegration;
 import crossover.CrossoverStrategy;
-import crossover.CyclicCrossover;
+import crossover.PMXCrossover;
 import fitness.FitnessGlobal;
 import fitness.FitnessStrategy;
 import geneticAlgorithm.CryptoProblem;
@@ -9,22 +14,46 @@ import parentSelection.TournamentSelection;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        LocalDateTime startTime;
+        LocalDateTime endTime;
         System.out.println("Iniciando sistema!");
 
         CryptoProblem currenProblem = new CryptoProblem("send", "more", "money");
 
-        CrossoverStrategy crossoverMethod = new CyclicCrossover();
+        CrossoverStrategy crossoverMethod = new PMXCrossover();
         ParentSelectionStrategy selectionParentMethod = new TournamentSelection();
         FitnessStrategy fitnessMethod = new FitnessGlobal(currenProblem);
+        ReintegrationStrategy reintegrationMethod = new SortededReintegration();
 
-        GenAlg algoritmoGenetico = new GenAlg(20, 100, 50, 60, crossoverMethod, selectionParentMethod, fitnessMethod);
+        GenAlg algoritmoGenetico = new GenAlg
+                                        (20, 
+                                        100, 
+                                        50, 
+                                        60, 
+                                        crossoverMethod, 
+                                        selectionParentMethod, 
+                                        fitnessMethod, 
+                                        reintegrationMethod);
+
+        startTime = LocalDateTime.now();                                    
         algoritmoGenetico.executeAlgorithm();
+        endTime = LocalDateTime.now();
 
         System.out.println(algoritmoGenetico.getLettersSequenc());
+        
+        System.out.println(algoritmoGenetico.getBestIndividual());
+        Duration duration = Duration.between(startTime, endTime);
+        System.out.println("Tempo de execução: " + duration.toMillis() + " ms");
+        
 
-        algoritmoGenetico.getPopulation().forEach(individual->{
-            System.out.println(individual);
-        });
+        // algoritmoGenetico.getPopulation().forEach(individual->{
+        //     System.out.println(individual);
+        // });
+
+        // algoritmoGenetico.getChildren().forEach(individual->{
+        //     System.out.println(individual);
+        // });
 
         System.out.println("Encerrando sistema!");
     }
