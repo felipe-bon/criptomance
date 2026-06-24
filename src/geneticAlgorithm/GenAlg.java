3213493bdb6e619cc9e49aa588aea79162bf3e5d
felipe-bon <felipebonetti2001@hotmail.com>
2026-06-23 22:42:36 -0300

    add crossover, reintegration, getPopulation, getBestIndividual and selectParents methods and othes atributes

--- src/geneticAlgorithm/GenAlg.java
+++ src/geneticAlgorithm/GenAlg.java
@@ -1,8 +1,12 @@
 package geneticAlgorithm;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import Reintegration.ReintegrationStrategy;
 import crossover.CrossoverStrategy;
 import fitness.FitnessStrategy;
 import parentSelection.ParentSelectionStrategy;
@@ -11,18 +15,23 @@ public class GenAlg {
 
   private int population_size;
   private int p_mutation;
-  private ArrayList<Individual> population;
   private CrossoverStrategy crossoverStrategy;
   private ParentSelectionStrategy parentSelectionStrategy;
   private FitnessStrategy fitnessStrategy;
+  private ReintegrationStrategy reintegrationStrategy;
   private int maxGeneration = 100;
-  private int crossoverRate = 60;
+  private int crossoverRate = 60; // em porcentagem
+  private ArrayList<Individual> population;
+  private ArrayList<Individual> parents;
+  private ArrayList<Individual> children;
 
-
-  public GenAlg(int p_mutation, int population_size, int maxGeneration, int crossoverRate, CrossoverStrategy crossoverStrategy, ParentSelectionStrategy parentSelectionStrategy, FitnessStrategy fitnessStrategy){
+  public GenAlg(int p_mutation, int population_size, int maxGeneration, int crossoverRate,
+      CrossoverStrategy crossoverStrategy, ParentSelectionStrategy parentSelectionStrategy,
+      FitnessStrategy fitnessStrategy, ReintegrationStrategy reintegrationStrategy) {
     this.crossoverStrategy = crossoverStrategy;
     this.parentSelectionStrategy = parentSelectionStrategy;
     this.fitnessStrategy = fitnessStrategy;
+    this.reintegrationStrategy = reintegrationStrategy;
     this.p_mutation = p_mutation;
     this.population_size = population_size;
     this.maxGeneration = maxGeneration;
@@ -31,30 +40,44 @@ public class GenAlg {
     this.generatePopulation();
   }
 
-  private void generatePopulation(){
+  private void generatePopulation() {
     // Gera população usando paralelismo
     this.population = IntStream.range(0, population_size)
-    .parallel()
-    .mapToObj(i -> new Individual(p_mutation))
-    .collect(Collectors.toCollection(ArrayList::new));
+        .parallel()
+        .mapToObj(i -> new Individual(p_mutation))
+        .collect(Collectors.toCollection(ArrayList::new));
   }
 
   // TODO
-  private void selectParent(){
-
+  private void selectParent() {
+    this.parents = this.parentSelectionStrategy.selectParents(population, crossoverRate);
     return;
   }
 
   // TODO
-  private void crossover(){
+  private void crossover() {
+    this.children = IntStream.range(0, parents.size() / 2)
+        .parallel()
+        .mapToObj(i -> {
+          Individual parent1 = population.get(i * 2);
+          Individual parent2 = population.get(i * 2 + 1);
 
-    return;
+          return crossoverStrategy.crossover(parent1, parent2);
+        })
+        .flatMap(Arrays::stream)
+        .collect(Collectors.toCollection(ArrayList::new));
+
+    calculatePopulationFitness(this.children);
+  }
+
+  private void reeintegration() {
+    this.population = reintegrationStrategy.reeintegration(population, children);
   }
 
   // Calcula o fitness da população usando paralelismo
-  private void calculatePopulationFitness(){
-    
-    this.population.parallelStream().forEach(individual -> {
+  private void calculatePopulationFitness(ArrayList<Individual> population) {
+
+    population.parallelStream().forEach(individual -> {
       int fitness = fitnessStrategy.calculateFitness(individual.getChromosome());
       individual.setFitnessValue(fitness);
     });
@@ -63,16 +86,36 @@ public class GenAlg {
   }
 
   // TODO
-  public void executeAlgorithm(){
-    calculatePopulationFitness();
-    return;
+  public void executeAlgorithm() {
+
+    int currentGenration = 0;
+
+    calculatePopulationFitness(population);
+    while (currentGenration < maxGeneration && getBestIndividual().getFitnessValue() > 0) {
+      selectParent();
+      crossover();
+      reeintegration();
+      currentGenration++;
+    }
   }
 
-  public ArrayList<Individual> getPopulation(){
+  public ArrayList<Individual> getPopulation() {
     return population;
   }
 
-  public String getLettersSequenc(){
+  public Individual getBestIndividual() {
+    return population.stream()
+        .min((i1, i2) -> Integer.compare(
+            i1.getFitnessValue(),
+            i2.getFitnessValue()))
+        .orElse(null);
+  }
+
+  public String getLettersSequenc() {
     return this.fitnessStrategy.getLettersSequenc();
+  }
+
+  public ArrayList<Individual> getChildren() {
+    return children;
   }
 }
